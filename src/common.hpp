3fe646a3bc1e4624fcfdb786fc3a6f151cc7948b
Yimura <andreas.maerten@scarlet.be>
2021-12-28 23:05:43 +0100

    feat(Common): Includes for ThreadPool and WSServer

--- src/common.hpp
+++ src/common.hpp
@@ -1,1 +1,25 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <stack>
+#include <thread>
+#include <vector>
+
+#include <csignal>
+
 #include <iostream>
+
+#include <map>
+
+#include "util/logger.hpp"
+#include "util/thread_pool.hpp"
+
+namespace distream
+{
+    using namespace std::literals::chrono_literals;
+
+    inline std::atomic<bool> g_running { true };
+}

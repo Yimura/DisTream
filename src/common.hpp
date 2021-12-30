98fd02a33d9ab3c6c711bcb57b8914f80e0b28bb
Yimura <andreas.maerten@scarlet.be>
2021-12-30 22:58:51 +0100

    feat(Common): Added settings and enums

--- src/common.hpp
+++ src/common.hpp
@@ -12,10 +12,17 @@
 
 #include <csignal>
 
+#include <fstream>
+
 #include <map>
+
+#include "nlohmann/json.hpp"
 
 #include "websocketpp/config/asio_no_tls.hpp"
 #include "websocketpp/server.hpp"
+
+#include "core/enums.hpp"
+#include "core/settings.hpp"
 
 #include "util/logger.hpp"
 #include "util/thread_pool.hpp"
@@ -25,11 +32,12 @@ namespace distream
     using namespace std::literals::chrono_literals;
 
     typedef websocketpp::server<websocketpp::config::asio> server;
-
     using websocketpp::connection_hdl;
     using websocketpp::lib::placeholders::_1;
     using websocketpp::lib::placeholders::_2;
     using websocketpp::lib::bind;
+
+    using nlohmann::json;
 
     inline std::atomic<bool> g_running { true };
 }

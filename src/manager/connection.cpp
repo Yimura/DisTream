7544e80983eddccfd6add9d7b2f7dd557165a478
Yimura <andreas.maerten@scarlet.be>
2021-12-30 23:02:06 +0100

    feat(Connection): Changes
    Keep connection_hdl stored in private member
    Added authenticate and is_authenticated methods

--- src/manager/connection.cpp
+++ src/manager/connection.cpp
@@ -2,7 +2,8 @@
 
 namespace distream
 {
-    Connection::Connection()
+    Connection::Connection(connection_hdl hdl)
+        : m_hdl(hdl)
     {
 
     }
@@ -10,5 +11,18 @@ namespace distream
     Connection::~Connection()
     {
         
+    }
+
+    void Connection::authenticate(std::string application_id, int shard_id)
+    {
+        m_application_id = application_id;
+        m_shard_id = shard_id;
+
+        m_authenticated = true;
+    }
+
+    bool Connection::is_authenticated()
+    {
+        return m_authenticated;
     }
 }
--- src/manager/connection.hpp
+++ src/manager/connection.hpp
@@ -1,14 +1,27 @@
+#pragma once
+#include "common.hpp"
+
 namespace distream
 {
     class Connection
     {
     private:
+        connection_hdl m_hdl; 
+
+        bool m_authenticated = false;
+
+        std::string m_application_id = "";
+        int m_shard_id = -1;
         // list of guilds
         // guild contains a link to a threaded player
 
     public:
-        Connection(/* args */);
+        Connection(connection_hdl hdl);
         ~Connection();
+
+        void authenticate(std::string application_id, int shard_id);
+
+        bool is_authenticated();
     };
     
 }
